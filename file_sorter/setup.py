211a9815b0c936d8839fc9296956ebd9b836a664
Lilian Luong <lilianluong64@gmail.com>
2020-09-19 15:08:08 -0700

    disable SSL check for nltk download setup

--- file_sorter/setup.py
+++ file_sorter/setup.py
@@ -8,6 +8,15 @@ def first_time_setup():
         pkl.dump({}, f)
 
     import nltk
+    import ssl
+
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+
     nltk.download('punkt')
     nltk.download('stopwords')
 
